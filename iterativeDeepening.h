43ddcc51e97f3e825bb524e70df7904dc97a26be
leodorna <leo.dorneles@hotmail.com>
2024-04-09 21:39:57 -0300

    fix header id_solver

--- iterativeDeepening.h
+++ iterativeDeepening.h
@@ -2,7 +2,7 @@
 #define IDS_H
 #include "tilepuzzle.h"
 
-void id_solver(const TILE_size& initial_state, int blank_x, int blank_y);
+void id_solver(const TILE_size& initial_state, const int &blank_x, const int &blank_y);
 
 int DFLS(const TILE_size &state, Move last_move, const int &blank_x, const int &blank_y, const int &d_limit, int depth, int &expanded_nodes);
 #endif
